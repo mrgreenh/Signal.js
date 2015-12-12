d045ee4b31f5e4e04bed3b1c283176aa7d2599df
Carlo Andrea Conte <carloandreaconte@icloud.com>
2015-12-11 19:56:15 -0800

    Offset module

--- src/modules/ModulesBag.js
+++ src/modules/ModulesBag.js
@@ -1,13 +1,15 @@
 import LowPass from './LowPass'
 import Multiplier from './Multiplier'
 import ADSRCurve from './ADSRCurve'
+import Offset from './Offset'
 
 class ModulesBag {
     static getModulesMap(){
         return {
                     [LowPass.MODULE_NAME]: LowPass,
                     [Multiplier.MODULE_NAME]: Multiplier,
-                    [ADSRCurve.MODULE_NAME]: ADSRCurve
+                    [ADSRCurve.MODULE_NAME]: ADSRCurve,
+                    [Offset.MODULE_NAME]: Offset
                 }
     }
 
--- src/modules/Offset.js
+++ src/modules/Offset.js
@@ -1,22 +1,22 @@
 import SignalModule from '../SignalModule'
 
-class Multiplier extends SignalModule {
+class Offset extends SignalModule {
 
     constructor(configuration){
         super(1);
-        this._factor = configuration.factor.value;
+        this._offset = configuration.offset.value;
     }
 
     _processOutput(){
-        return this._buffer.getFirstItem()*parseFloat(this._factor);
+        return this._buffer.getFirstItem()+parseFloat(this._offset);
     }
 
     static getConfigurationSchema(){
         var conf = super.getConfigurationSchema();
         return Object.assign(conf, {
-            type: Multiplier.MODULE_NAME,
-            factor: {
-                display: "Factor",
+            type: Offset.MODULE_NAME,
+            offset: {
+                display: "Offset",
                 type: "number",
                 range: [-1000,10000],
                 value: 1
@@ -25,6 +25,6 @@ class Multiplier extends SignalModule {
     }
 }
 
-Multiplier.MODULE_NAME = "Multiplier";
+Offset.MODULE_NAME = "Offset";
 
-export default Multiplier;
+export default Offset;
