0f2c11845033ab91ade4ab4aff9c32a9a2ee086b
Carlo Andrea Conte <carloandreaconte@icloud.com>
2016-01-25 21:54:25 -0800

    first jekyll commit

---
+++ _site/src/SignalModule.js
@@ -0,0 +1,47 @@
+import SignalBuffer from './SignalBuffer'
+
+class SignalModule {
+    constructor(bufferSize){ //Update to accept a configuration object
+        this._buffer = new SignalBuffer(bufferSize);
+    }
+
+    queueSample(value){
+        if(isNaN(value)) throw "Signals only accept numbers";
+        if(this.hasOwnProperty("_chainedModule")){
+            let backPropagatedValue = this._chainedModule.queueSample(value);
+            this._buffer.push(backPropagatedValue);
+        }else{
+            this._buffer.push(value);
+        }
+
+        return this.output();
+    }
+
+    output(){
+        return this._processOutput();
+    }
+
+    _processOutput(){
+        return this._buffer.getFirstItem() || 0;
+    }
+
+    chain(module){
+        this._chainedModule = module;
+        module.addChainPredecessor(this);
+        return module;
+    }
+
+    addChainPredecessor(module){
+        this._chainPredecessor = module;
+    }
+
+    getConfigurationSchema(){
+        return {};
+    }
+
+    static getConfigurationSchema(){
+        return {};
+    }
+}
+
+export default SignalModule;
